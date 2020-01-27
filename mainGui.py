139efb3b57b6bfff6c9a1e6fa145b3a129697d21
Dario Protulipac <d.protulipac@vipnet.hr>
2020-01-27 16:24:58 +0100

    GUI started

--- mainGui.py
+++ mainGui.py
@@ -1,12 +1,56 @@
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QVBoxLayout
+from PyQt5.QtWidgets import QAction, QToolBar, QStatusBar
+from PyQt5.QtGui import QPalette, QColor
 import sys
+import socket
 
-from PySide2.QtWidgets import QApplication, QLabel
+def getIPAddress():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('2.2.2.2', 1))
+    return s.getsockname()[0]
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+class MainWindow(QMainWindow):
 
-    label = QLabel("Hello World")
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("Test Qt app")
+        layout = QVBoxLayout()
+        layout.addWidget(Color('red'))
+        layout.addWidget(Color('blue'))
+        layout.addWidget(Color('green'))
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        toolbar = QToolBar("Main toolbar")
+        self.addToolBar(toolbar)
 
-    label.show()
+        button_action = QAction("User", self)
+        button_action.triggered.connect(self.onToolBarButton1Click)
+        button_action.setCheckable(True)
+        toolbar.addAction(button_action)
 
-    sys.exit(app.exec_())
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+
+    def onToolBarButton1Click(self, s):
+        print("click", s)
+
+
+class Color(QWidget):
+
+    def __init__(self, color, *args, **kwargs):
+        super(Color, self).__init__(*args, **kwargs)
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.statusBar.showMessage("Your IP is: " + str(getIPAddress()))
+window.resize(480, 600)
+window.show()
+
+app.exec_()
